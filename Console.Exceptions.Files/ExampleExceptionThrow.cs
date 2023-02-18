15004dfa4380d9170493b5f45b8663ce610812a3
vlad <vlad19972017@gmail.com>
2023-02-18 21:38:53 +0200

    Code.

---
+++ Console.Exceptions.Files/ExampleExceptionThrow.cs
@@ -0,0 +1,24 @@
+﻿namespace Console.Exceptions.Files;
+
+using System;
+
+public class ExampleExceptionThrow
+{
+    public void Logic()
+    {
+        new FileService();
+        new FileService();
+        new FileService();
+
+        var objectException = new DivideByZeroException("Exception with LogicInvalid function.");
+        
+        Console.WriteLine(".....");
+        LogicInvalid();
+    }
+
+    public void LogicInvalid()
+    {
+        Console.WriteLine("LogicInvalid function");
+        throw new MyException() { MyInfo = "123123" };
+    }
+}
---
+++ Console.Exceptions.Files/FileService.cs
@@ -0,0 +1,23 @@
+﻿namespace Console.Exceptions.Files;
+
+using System;
+using System.IO;
+
+public class FileService : IDisposable
+{
+    private readonly StreamWriter _streamWriter = new StreamWriter("text.txt");
+
+    public FileService() { }
+
+    ~FileService()
+    {
+        _streamWriter.Dispose();
+        Console.WriteLine("Finalizer of FileService");
+    }
+
+    public void Dispose()
+    {
+        _streamWriter.Dispose();
+    }
+}
+
